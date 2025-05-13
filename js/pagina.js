26f185fd88f8f067e80795bebc9ce8d0ff5a1578
vitor <vitinho38.martins@gmail.com>
2025-05-13 19:36:29 -0300

    teste

---
+++ js/pagina.js
@@ -0,0 +1,12 @@
+function login() {
+    alert ("cliquei");
+}
+const email = document.getElementById("sim").value
+const senha = document.getElementById("senha").value
+
+ if (email == "antoniobuxa123@gmail.com" && senha == "antonio123"){
+    alert ("login com sucesso")
+} else {
+  
+     alert ("usuario nao encontrado")
+ }
---
+++ js/script.js
@@ -0,0 +1,11 @@
+function exemplo01() {
+    const text1 = parseFloat (document.getElementById("nome").value);
+    const text2 = parseFloat (document.getElementById("idade").value);
+    alert (`${text1} possui ${text2} anos`);
+    const rf = text1 + text2   
+
+    if (rf > 3000)
+    alert(passou)
+    
+
+} 
